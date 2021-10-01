0e79cea755ecadf72a8557357b3f146eb99f0210
akjolajo <akzolergesbajuulu795@gmail.com>
2021-10-01 08:39:40 +0300

    hw3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        //ctrl + K = commit
 	// write your code here
+        System.out.println("salam");
     BankAccount forakjolaccount = new BankAccount();
     forakjolaccount.deposit(20000);
     System.out.println("пополнение: ваш баланс " + forakjolaccount.getAmount());
